21146110eb4a8a4d43dc9bc11273e6e427c551c7
larah beatriz <larahbsb06@gmail.com>
2025-11-28 15:43:30 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double percentualBonus, percentualRetorno;
+int pontosAtransferir;
+int pontosAReduzirOigem, pontosAAcrescentarDestino;
+
+Console.WriteLine("---Milhas Bumerangue---\n");
+
+Console.Write("Percentual de bônus....: ");
+percentualBonus = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Percentual de retorno....: ");
+percentualRetorno = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Pontos a tansferir....: ");
+pontosAtransferir = Convert.ToInt32(Console.ReadLine());
+
+pontosAReduzirOigem = Convert.ToInt32(pontosAtransferir - (percentualRetorno / 100) * pontosAtransferir);
+pontosAAcrescentarDestino = Convert.ToInt32(pontosAtransferir + (percentualBonus /100) * pontosAtransferir);
+
+Console.WriteLine();
+Console.WriteLine($"Pontos a reduzir na origem......: {pontosAReduzirOigem}");
+Console.WriteLine($"Pontos a acrescentar no destino...: {pontosAAcrescentarDestino}");
